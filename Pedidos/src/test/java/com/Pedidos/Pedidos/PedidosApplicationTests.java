11fbd11a1833d2664a361f07ce866f4924ffe1ab
Ricardo Henrique Silva de Souza <ricardohenriquesilva286@gmail.com>
2026-03-22 11:25:53 -0300

    Initial commit

---
+++ Pedidos/src/test/java/com/Pedidos/Pedidos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pedidos.Pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
